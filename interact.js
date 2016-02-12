dd7e9a445942f8e5d408be8e23f87850a7de8cc6
Dane Tomseth <danetomseth@gmail.com>
2016-02-12 01:28:54 -0800

    getting there

--- interact.js
+++ interact.js
@@ -2,9 +2,11 @@
 // try to elminate these global variables in your project, these are here just to start.
 
 
-var winningNumber = Math.ceil(Math.random() * 100); //Generates random decimal between 0-1
+var winningNumber = Math.ceil(Math.random() * 100);
+
 var playerGuess;
 var guessCount = 0;
+var hintTaken = false;
 
 /*$(document).keypress(function(key) {
     if(key.which == 13) {
@@ -21,11 +23,38 @@ $(document).ready(function() {
     $(this).val("");
   });
   
+  	$('#giveHint').on('click', function() {
+		var numString = winningNumber.toString();
+		var randomDigit = Math.ceil(Math.random() * 2) - 1;
+		var numLength = numString.length;
+		if(numLength === 1) {
+			
+			$('#hint').show();
+			$('#hint').find('#hint-text').text('Only One Digit!');
+		}
+		else if(!hintTaken) {
+    	
+		$('#hint').show();
+		$('#hint').find('#hint-text').text('One of the digits is: ' + numString[randomDigit]);
+		hintTaken = true;
+		}
+		else {
+			$('#hint').find('#hint-text').text('Nobody Likes a Cheater!');
+		}
+		
+		
+  });
+  
   	$('#submitGuess').on('click', function() {
     	//playerGuess = +$('#playerNumber').val();
 		//$('#checkInput').text(playerGuess);
 		guessEntered();
   });
+  
+  
+  	$('#restart').click(function() {
+    location.reload();
+	});
   
   
 	
@@ -34,9 +63,7 @@ $(document).ready(function() {
 		
 		if(key.which == 13) {
 			key.preventDefault();
-    	//playerGuess = +$(this).val();
-		//$('#checkInput').text(playerGuess);
-		guessEntered();
+			guessEntered();
 		}
     //var dailyPrice = +$(this).closest('.tour').data('daily-price');
     
@@ -66,6 +93,12 @@ function guessEntered() {
 }
 
 function highOrLow(number) {
+	//$(".Text").css('margin-top', vertAlign);
+	var distanceOff = Math.abs(winningNumber - number) * 5;
+	$('#status-bar').show();
+	$('#status-bar').css('width', distanceOff);
+	
+	
 	if(number < winningNumber) {
 		resultBox('low');
 	}
