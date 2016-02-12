bd75d6f6bde3f10f42e2e4556fc1e021241fd991
Dane Tomseth <danetomseth@gmail.com>
2016-02-11 17:34:30 -0800

    Add updated files and jquery

--- interact.js
+++ interact.js
@@ -1,32 +1,129 @@
 /* **** Global Variables **** */
 // try to elminate these global variables in your project, these are here just to start.
-var winningNumber = Math.ceil(Math.random() * 100); //Generates random decimal between 0-1
-$(document).ready(
 
-function lowerOrHigher(){
-	if(winningNumber < 50) {
-		$(".input-Text").text("lower");
-	}
-	else {
-		$(".input-Text").text("higher");
+
+var winningNumber = Math.ceil(Math.random() * 100); //Generates random decimal between 0-1
+var playerGuess;
+var guessCount = 0;
+
+/*$(document).keypress(function(key) {
+    if(key.which == 13) {
+        alert('You pressed enter!');
+    }
+});*/
+
+$(document).ready(function() {
+	$('#playerNumber').val('Input Number');
+	$('#correctNumber').text("Winning number: "+winningNumber);
+	$('#guessNumber').text("Guess Left: "+(5- guessCount));
+	
+	$('#playerNumber').on('click', function() {
+    $(this).val("");
+  });
+  
+  	$('#submitGuess').on('click', function() {
+    	//playerGuess = +$('#playerNumber').val();
+		//$('#checkInput').text(playerGuess);
+		guessEntered();
+  });
+  
+  
+	
+	
+	$('#playerNumber').keypress(function(key) {
+		if(key.which == 13) {
+    	//playerGuess = +$(this).val();
+		//$('#checkInput').text(playerGuess);
+		guessEntered();
+		}
+    //var dailyPrice = +$(this).closest('.tour').data('daily-price');
+    
+  });
+  /*$('#playerNumber').on('focus', function() {
+    $('#playerNumber').val(99);
+  });*/
+});
+
+function guessEntered() {
+	playerGuess = +$('#playerNumber').val();
+	guessCount++;
+	$('#checkInput').text("Last Guess: "+playerGuess);
+	$('#guessNumber').text("Guess Left: "+(5- guessCount));
+	
+	
+	highOrLow(playerGuess);
+	
+	checkGuess();
+	if(guessCount >= 5) {
+		gameOver();
 	}
 }
 
-)
+function highOrLow(number) {
+	if(number < winningNumber) {
+		resultBox('low');
+	}
+	else if(number > winningNumber) {
+		resultBox('high');
+	}
+	else if(number === winningNumber) {
+		alert('You won');
+	}
+}
 
 
-$(document).ready(
+
+function resultBox(result) {
+	if(result == 'low') {
+		$('#guessResult').css({'background-color': 'red'});
+		$('#guessResult').show();
+		$('#guessResult').find('#guessHighlight').text('Too Low');
+	}
+	else if(result == 'high') {
+		$('#guessResult').css({'background-color': 'green'});
+		$('#guessResult').show();
+		$('#guessResult').find('#guessHighlight').text('Too High');
+		
+	}
+}
+
+function gameOver() {
+	
+}
+
+
+/*function fetchGuess() {
+	$('guessNumber').keypress(function() {
+		playerGuess = $(this).val();
+		alert("Keypress");
+	});
+};
+
+function changeText () {
+	$('.input-wrapper').on('click' , function()    {
+            lowerOrHigher();
+			
+        });	
+};*/
+
+
+/*function checkGuess() {
+	alert("here");
+	playerGuess = $(this).val();
+	
+};*/
+
 
 function generateWinningNumber (){
     
 	$('#guessNumber').val(winningNumber);
-}
+};
 
 
 
 
 
-)
+
 
 
 /* **** Global Variables **** */
